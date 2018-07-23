bb38a170574a90808be69eec0ecc274f4d7edc7d
Jake <yucj@dxy.cn>
2018-07-23 09:50:05 +0800

    feat: 添加scss支持

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,17 @@ module.exports = {
           'css-loader'
         ]
       }, {
+        test: /\.scss$/,
+        use: [{
+          loader: 'style-loader' // creates style nodes from JS strings
+        },
+        {
+          loader: 'css-loader' // translates CSS into CommonJS
+        },
+        {
+          loader: 'sass-loader' // compiles Sass to CSS
+        }]
+      }, {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: {
